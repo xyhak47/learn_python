9de1f7040b158cf2a8f76b37dda6e76a9c4d439c
xyhak47 <474318838@qq.com>
2016-08-30 14:57:04 +0800

    Update FileLineCounter.py

--- patent_tool/FileLineCounter.py
+++ patent_tool/FileLineCounter.py
@@ -1,1 +1,44 @@
+# Author: xyh
+# Date: 2016.8.26
+# File line counter
 
+import sys, os
+
+totalLines = 0
+filePathDir = []
+
+def handle_all_files():
+    global totalLines
+    for filePath in filePathDir:
+        totalLines += sum(1 for line in open(filePath))
+
+
+def deep_iterate_dir(rootDir):
+    for lists in os.listdir(rootDir):
+        path = os.path.join(rootDir, lists)
+        if os.path.isdir(path):
+            deep_iterate_dir(path)
+        elif os.path.isfile(path):
+            if isCsFile(path):
+                filePathDir.append(path)
+                print path + '\n'
+
+
+def isCsFile(path):
+    return os.path.splitext(path)[1] == '.cs'
+
+
+def main(argv):  
+    targetPath = argv[0]
+    print 'targetPath = ' + targetPath     
+
+    print '******************** all cs file path begin **************************'
+    deep_iterate_dir(targetPath) 
+    print '******************** all cs file path end **************************'
+  
+    handle_all_files() 
+    print 'totalLines = ' +  str(totalLines)
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
